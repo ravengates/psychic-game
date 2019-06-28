c84cc805b34d0faae6c8b65267754d54986e06e3
Raven Gates <ravengates@icloud.com>
2019-06-28 16:32:20 -0500

    added all letters of the alphabet to the computer choices just for fun

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //array of options for the computer
-var computerChoices = ["t", "z", "s", "p", "m", "w", "f", "q", "r"];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var wins = 0;
 var losses = 0;
 var GuessesLeft = 9;
